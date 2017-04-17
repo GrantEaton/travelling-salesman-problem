d5f9108728046f8fc24c7fb9cd47159f1e3b377a
Grant Eaton <grantman100@gmail.com>
2017-04-17 16:15:43 -0400

    got it graphing properly finally

--- TSP.py
+++ TSP.py
@@ -5,11 +5,27 @@ import matplotlib.pyplot as plt
 
 G=nx.Graph()
 graphTups = parser.get_cities_tups();
-G.add_edges_from(graphTups);
+
+
+count = 1
+for point in graphTups:
+    G.add_node(count, pos = (point[0],point[1]))
+    count+=1
 print(G.nodes())
-nx.draw(G)
+#G.add_edges_from(graphTups);
+nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=300)
+#nx.draw(G)
 plt.show()
 
-#def BruteForceTSP
+#string bestPath
+#int bestScore
 
-
+def BruteForceTSP (edges, start, curNode, path, score, visited):
+    
+    for node in edges[curNode]:
+        if(node == start and len(visited) == len(edges) and score > bestScore):
+            bestPath = path
+            bestScore = score
+        else:
+            visited[curNode] = 1
+            #BruteForceTSP(edges, node, (path + str(node)), 
